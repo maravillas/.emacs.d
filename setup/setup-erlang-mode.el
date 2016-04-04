a96744ff275185d74d4799f962eb96004f08379e
Matthew Maravillas <maravillas@transcend.local>
2016-04-04 13:49:12 -0400

    erlang mode space fix

--- setup/setup-erlang-mode.el
+++ setup/setup-erlang-mode.el
@@ -16,6 +16,15 @@
 
 (add-hook 'erlang-mode-hook 'rebar-erlang-mode-hook)
 
+(defun my/disable-paredit-spaces-before-paren ()
+  ;; Function which always returns nil -> never insert a space when insert a parentheses.
+  (defun my/erlang-paredit-space-for-delimiter-p (endp delimiter) nil)
+  ;; Set this function locally as only predicate to check when determining if a space should be inserted
+  ;; before a newly created pair of parentheses.
+  (setq-local paredit-space-for-delimiter-predicates '(my/erlang-paredit-space-for-delimiter-p)))
+
+(add-hook 'erlang-mode-hook 'my/disable-paredit-spaces-before-paren)
+
 (provide 'setup-erlang-mode)
 
 
